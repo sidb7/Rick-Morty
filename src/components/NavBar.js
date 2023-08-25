ea9e2a12a9b3340da8018ef7bc23458d9e8bc1fb
Siddhant <bhandarysid@gmail.com>
2023-08-25 22:33:19 +0530

    ain

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
     <div className='w-100 d-flex justify-content-center'>
-        <img  style={{width:"25rem"}} src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Logo.png" alt="" />
+        <img  style={{width:"25rem",userSelect:'none'}} src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Logo.png" alt="" />
         </div>
  
   )
